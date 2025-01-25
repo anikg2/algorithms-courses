e8d726e01e5f7bc41c144b88e02b5299bee18d71
Anik Ghosh <anik.ghosh@rwth-aachen.de>
2025-01-25 01:57:40 +0100

    More progress with assignment 2

--- Algorithms I/Module 2/Percolation.java
+++ Algorithms I/Module 2/Percolation.java
@@ -1,9 +1,3 @@
-/* *****************************************************************************
- *  Name:              Ada Lovelace
- *  Coursera User ID:  123456
- *  Last modified:     October 16, 1842
- **************************************************************************** */
-
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
@@ -28,25 +22,47 @@ public class Percolation {
         // 0 to n^2-1 for the grid, and the last two for source and sink
         this.uf = new WeightedQuickUnionUF(n * n + 2);
 
-        // Now, initialize the grid and select a random cell and open it
+        // Now, initialize the grid with all cells closed
         this.grid = new int[n][n];
-        for (int i = 0; i < n * n; i++) {
-            int randomRow = StdRandom.uniformInt(0, n);
-            int randomCol = StdRandom.uniformInt(0, n);
+
+        // Finally, open a cell at random until the system percolates
+        while (!this.percolates()) {
+
+            int randomRow = StdRandom.uniformInt(1, n + 1);
+            int randomCol = StdRandom.uniformInt(1, n + 1);
             if (this.isOpen(randomRow, randomCol)) {
                 continue;
             }
             else {
+                if (this.openSites == 50) {
+                    break;
+                }
                 this.openSites++;
                 this.open(randomRow, randomCol);
+                // If it is in the top row, union with the source
+                if (randomRow == 1) {
+                    this.uf.union(0, randomCol);
+                }
+
+                // If it is in the bottom row, union with the sink
+                if (randomRow == n) {
+                    this.uf.union((n * n) + 1, ((randomRow - 1) * n) + randomCol);
+                }
+
+                // Debugging to check if union works
+                this.uf.union(0, ((randomRow - 1) * n) + randomCol);
+                // Check the parents
+                int parent0 = this.uf.find(0);
+                int parent1 = this.uf.find(((randomRow - 1) * n) + randomCol);
+                boolean a = parent0 == parent1;
+                System.out.println(a);
             }
         }
-
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (row <= 0 || row >= this.gridSize || col <= 0 || col >= this.gridSize) {
+        if (row <= 0 || row > this.gridSize || col <= 0 || col > this.gridSize) {
             throw new IllegalArgumentException("Invalid argument(s) passed to function");
         }
         this.grid[row - 1][col - 1] = 1;
@@ -54,7 +70,7 @@ public class Percolation {
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        if (row <= 0 || row >= this.gridSize || col <= 0 || col >= this.gridSize) {
+        if (row <= 0 || row > this.gridSize || col <= 0 || col > this.gridSize) {
             throw new IllegalArgumentException("Invalid argument(s) passed to function");
         }
         return this.grid[row - 1][col - 1] == 1;
@@ -62,11 +78,11 @@ public class Percolation {
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        if (row <= 0 || row >= this.gridSize || col <= 0 || col >= this.gridSize) {
+        if (row <= 0 || row > this.gridSize || col <= 0 || col > this.gridSize) {
             throw new IllegalArgumentException("Invalid argument(s) passed to function");
         }
         // If the element at the given index is part of the same set as the source, return true
-        return this.uf.find(//Fix this) == this.uf.find(this.gridSize*this.gridSize);
+        return this.uf.find(((row - 1) * this.gridSize) + col) == this.uf.find(0);
     }
 
     // returns the number of open sites
@@ -77,13 +93,16 @@ public class Percolation {
     // does the system percolate?
     public boolean percolates() {
         // If source and sink are part of the same set, the system percolates
-        return this.uf.find(this.gridSize * this.gridSize) == this.uf.find(
-                this.gridSize * this.gridSize + 1);
+        return this.uf.find(0) == this.uf.find(this.gridSize * this.gridSize + 1);
     }
 
     // test client (optional)
     public static void main(String[] args) {
-
+        Percolation test = new Percolation(50);
+        // Number of open sites
+        System.out.println(test.numberOfOpenSites());
+        // Does the system percolate?
+        System.out.println(test.percolates());
     }
 
 }
--- Algorithms I/Module 2/PercolationStats.java
+++ Algorithms I/Module 2/PercolationStats.java
@@ -1,9 +1,35 @@
-/* *****************************************************************************
- *  Name:              Ada Lovelace
- *  Coursera User ID:  123456
- *  Last modified:     October 16, 1842
- **************************************************************************** */
-
 public class PercolationStats {
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("Invalid value of n or trials");
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return 2.0;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return 2.0;
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return 2.0;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return 2.0;
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+        
+    }
 
 }
